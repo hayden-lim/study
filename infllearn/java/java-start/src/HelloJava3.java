273364ab7591cd3083bee702ed6b7ee58fcbcbfa
hayden-lim <hahyunlim.90@gmail.com>
2023-12-30 23:20:08 +0900

    Update HelloJava3.java

--- infllearn/java/java-start/src/HelloJava3.java
+++ infllearn/java/java-start/src/HelloJava3.java
@@ -1,5 +1,9 @@
 public class HelloJava3 {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
     }
 }
