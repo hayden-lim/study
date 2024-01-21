27b2e9c6effd918c93d5c0e2308d16e13dcd8da0
hayden-lim <hahyunlim.90@gmail.com>
2024-01-21 22:33:56 +0900

    Update ArrayDi3.java

--- infllearn/java/java-start/src/array/ArrayDi3.java
+++ infllearn/java/java-start/src/array/ArrayDi3.java
@@ -9,8 +9,8 @@ public class ArrayDi3 {
         }; //행2, 열3
 
 
-        for (int row = 0; row < 2; row++) {
-        for (int column = 0; column < 3; column++){
+        for (int row = 0; row < arr.length; row++) {
+        for (int column = 0; column < arr[row].length; column++){
             System.out.print(arr[row][column] + " ");
         }
             System.out.println(); //한 행이 끝나면 라인을 변경한다.
