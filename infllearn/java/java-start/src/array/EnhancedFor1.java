d562e3ab9501a6d2abc62a8ed0e2aac1d7969fa7
hayden-lim <hahyunlim.90@gmail.com>
2024-01-22 22:26:03 +0900

    20240122
    quiz 1

--- infllearn/java/java-start/src/array/EnhancedFor1.java
+++ infllearn/java/java-start/src/array/EnhancedFor1.java
@@ -19,7 +19,7 @@ public class EnhancedFor1 {
             System.out.println(number);
         }
 
-        //for-each 문을 사용할 수 없는 경우, 증가하는 index 값이 필요할 때
+        //for-each 문을 사용할 수 없는 경우, 증가하는 index 값을 직접적으로 사용해야 하는 경우 사용 불가
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("number" + i + "번의 결과는: " + numbers[i]);
         }
