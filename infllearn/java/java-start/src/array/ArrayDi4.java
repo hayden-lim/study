b716f28ef04abfe43368b73e3b95e32101bb6865
hayden-lim <hahyunlim.90@gmail.com>
2024-01-22 22:10:45 +0900

    20240122
    향상된 for 문(for~each문)

--- infllearn/java/java-start/src/array/ArrayDi4.java
+++ infllearn/java/java-start/src/array/ArrayDi4.java
@@ -1,12 +1,15 @@
 package array;
 
-public class ArrayDi3 {
+public class ArrayDi4 {
     public static void main(String[] args) {
         // 2*3 2차원 배열 생성
-        int[][] arr = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+        int[][] arr = new int[4][5];
+
+        int i = 1;
+        for (int row = 0; row < arr.length; row++) {
+            for (int column =0; column < arr[row].length; column++) {
+                arr[row][column] = i++;
+            }
         }; //행2, 열3
 
 
