78640e6aaebea2d3e84146f77869791dac9a5895
hayden-lim <hahyunlim.90@gmail.com>
2024-01-25 23:47:16 +0900

    20240125

--- infllearn/java/java-start/src/method/Method1.java
+++ infllearn/java/java-start/src/method/Method1.java
@@ -7,13 +7,13 @@ public class Method1 {
         int b = 2;
         System.out.println(a + "+" + b + " 연산 수행");
         int sum1 = a + b;
-        System.out.println("결과 1 출력: " + sum1);
+        System.out.println(" 결과 1 출력 : " + sum1);
 
         // 계산 2
         int x = 10;
         int y = 20;
         System.out.println(x + "+" + y + " 연산 수행");
         int sum2 = x + y;
-        System.out.println("결과 2 출력: " + sum2);
+        System.out.println(" 결과 2 출력 : " + sum2);
     }
 }
