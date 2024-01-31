e99f76c0758a0bca0177663885b046bad91833c5
hayden-lim <hahyunlim.90@gmail.com>
2024-01-31 22:43:06 +0900

    Update MehodEx2.java

--- infllearn/java/java-start/src/ex/MehodEx2.java
+++ infllearn/java/java-start/src/ex/MehodEx2.java
@@ -2,25 +2,17 @@ package ex;
 
 public class MehodEx2 {
     public static void main(String[] args) {
-        String message = "Hello, world!";
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println(message);
-        }
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println(message);
-        }
+        printMessage("Hello, world!",3);
+        printMessage("java",5);
+        printMessage("Hi",7);
 
 
-        for (int i = 0; i < 7; i++) {
-            System.out.println(message);
-        }
     }
 
     public static void printMessage(String message, int times) {
         for (int i = 0; i < times; i++) {
-            System.out.println("message");
+            System.out.println(message);
         }
     }
 }
