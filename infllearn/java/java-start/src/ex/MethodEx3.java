7d4b2e178ce7670bb126eb37d433864d96a244f2
hayden-lim <hahyunlim.90@gmail.com>
2024-02-01 20:12:20 +0900

    20240201
    입금출금 메서드 만들기(예제)

---
+++ infllearn/java/java-start/src/ex/MethodEx3.java
@@ -0,0 +1,26 @@
+package ex;
+
+public class MethodEx3 {
+    //입출금 메서드 만들기
+    public static void main(String[] args) {
+        int balance = 10000;
+
+        //입금 1000
+        int depositAmount = 1000;
+        balance += depositAmount;
+        System.out.println(depositAmount + "원을 입금하였습니다. 현재 잔액: " + balance + "원 입니다. ");
+
+        //출금 2000
+        int withdrawAmount = 2000;
+        if (balance >= withdrawAmount) {
+            balance -= withdrawAmount;
+            System.out.println(withdrawAmount + "원을 출금하였습니다. 현재 잔액: " + balance + "원 입니다. ");
+
+        }else{
+            System.out.println(withdrawAmount + "원을 출금할 수 없습니다. 잔액이 부족합니다. ");
+
+        }
+        System.out.println("최종 잔액: " + balance + "원 입니다. ");
+    }
+
+}
