fd094718a74918f5d168d96de153ea17cdfc3afa
hayden-lim <hahyunlim.90@gmail.com>
2024-02-05 21:29:18 +0900

    20240205
    hello java 1 2 3

--- infllearn/java/java-start/src/start/HelloJava.java
+++ infllearn/java/java-start/src/start/HelloJava.java
@@ -2,6 +2,6 @@ package start;
 
 public class HelloJava { // 중괄호로 클래스의 시작과 끝을 나타냄
     public static void main(String[] args) { //main method 라고 한다. main 프로그램의 시작점이라고 단순히 이해하면 된다.
-        System.out.println("hello java!");
+        System.out.println("hello java!"); //자바는 문장의 끝을 ; 세미콜론으로 구분한다.
     } // 메서드의 시작과 끝
-}
+} // 클래스 범위의 끝 (중괄호의 끝)
--- infllearn/java/java-start/src/start/HelloJava2.java
+++ infllearn/java/java-start/src/start/HelloJava2.java
@@ -2,8 +2,8 @@ package start;
 
 public class HelloJava2 {
     public static void main(String[] args) {
-        System.out.println("hello java1");
-        System.out.println("hello java2");
-        System.out.println("hello java3");
+        System.out.println(" hello Java1 ");
+        System.out.println(" hello Java2 ");
+        System.out.println(" hello Java3 ");
     }
 }
