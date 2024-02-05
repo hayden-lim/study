9a34b3ac8db9d448ec209f8dfc4b024ffc3a9cf8
hayden-lim <hahyunlim.90@gmail.com>
2024-02-05 21:24:07 +0900

    Update HelloJava.java

--- infllearn/java/java-start/src/start/HelloJava.java
+++ infllearn/java/java-start/src/start/HelloJava.java
@@ -1,7 +1,7 @@
 package start;
 
-public class HelloJava {
-    public static void main(String[] args) {
+public class HelloJava { // 중괄호로 클래스의 시작과 끝을 나타냄
+    public static void main(String[] args) { //main method 라고 한다. main 프로그램의 시작점이라고 단순히 이해하면 된다.
         System.out.println("hello java!");
-    }
+    } // 메서드의 시작과 끝
 }
