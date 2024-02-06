7e0b6cebd619ae2bf70dceae7dc387f64527446a
hayden-lim <hahyunlim.90@gmail.com>
2024-02-06 22:16:34 +0900

    20240206
    주석처리 복습

--- infllearn/java/java-start/src/start/CommentJava.java
+++ infllearn/java/java-start/src/start/CommentJava.java
@@ -5,9 +5,10 @@ import java.sql.SQLOutput;
 
 public class CommentJava {
     public static void main(String[] args) {
+        //주석
         System.out.println("hello java1"); //hello java1을 출력합니다.
         //System.out.println("hello java2");
-
+        // 한줄주석 한 줄만 주석으로 처리
         /*
         System.out.println("hello java2");
         System.out.println("hello java3");
