d3088f6d39e4f954c77dca5f719aad01f0448878
hayden-lim <hahyunlim.90@gmail.com>
2024-02-07 21:47:41 +0900

    20240207
    변수 복습

--- infllearn/java/java-start/src/variable/Var1.java
+++ infllearn/java/java-start/src/variable/Var1.java
@@ -1,10 +1,12 @@
-package variable;
+package variable; // 소속된 패키지 선언
 
 public class Var1 {
     public static void main(String[] args) {
-        System.out.println(20); //변경 10->20
-        System.out.println(20); //변경 10->20
-        System.out.println(20); //변경 10->20
+        // 변수 (variable)
+
+        System.out.println(10); //변경 10->20->10
+        System.out.print(10); //변경 10->20->10, println -> print로 변경(줄바꿈 없음)
+        System.out.println(10); //변경 10->20
 
     }
 }
