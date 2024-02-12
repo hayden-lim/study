a0af1aa2d293f5aca69ab5390a0f6ea402dd8aea
hayden-lim <hahyunlim.90@gmail.com>
2024-02-12 23:19:28 +0900

    20240212

--- infllearn/java/java-start/src/ex/VarEx3.java
+++ infllearn/java/java-start/src/ex/VarEx3.java
@@ -1,14 +1,15 @@
 package ex;
 
 public class VarEx3 {
+
     public static void main(String[] args) {
-        long longVar = 1000000L;
+        long longVar = 10000000000L; // long 형 이라 뒤에 L 붙임, 기본적으로 리터럴은 int형이기 때문
         System.out.println(longVar);
 
         boolean booleanVar = true;
         System.out.println(booleanVar);
-
-
-
     }
+
+
+
 }
