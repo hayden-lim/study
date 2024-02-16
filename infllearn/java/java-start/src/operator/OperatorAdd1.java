22e0533a695becad9dd212945488a02cf5846e8a
hayden-lim <hahyunlim.90@gmail.com>
2024-02-16 23:34:59 +0900

    Update OperatorAdd1.java

--- infllearn/java/java-start/src/operator/OperatorAdd1.java
+++ infllearn/java/java-start/src/operator/OperatorAdd1.java
@@ -26,7 +26,8 @@ public class OperatorAdd1 {
         System.out.println("a = " + a);
 
         //증감 연산자는 피연산자(a) 앞, 뒤에 모두 둘 수 있음
-        // ++a 전위 증감 연산자 (Prefix)
-        // a++ 후위 증감 연산자 (Postfix)
+        //위치에 따라 연산이 수행되는 시점이 달라짐
+        // ++a 전위 증감 연산자 (Prefix) = 값을 먼저 증가시킴
+        // a++ 후위 증감 연산자 (Postfix) = 변수를 쓰고 그 다음에 증가시킴
     }
 }
