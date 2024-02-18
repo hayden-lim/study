bbff9dbdf702bf3f30996c45d10098c38e9d60cb
hayden-lim <hahyunlim.90@gmail.com>
2024-02-18 23:26:18 +0900

    20240218
    비교연산자 (숫자, 문자열)

--- infllearn/java/java-start/src/operator/Comp2.java
+++ infllearn/java/java-start/src/operator/Comp2.java
@@ -1,6 +1,6 @@
 package operator;
 
-public class Com2 {
+public class Comp2 {
     public static void main(String[] args) {
         //문자열 비교 할 땐 ==이 아니라 .equals()메서드를 사용해야 함
         // == 사용시 비교가 가능할 때도 있는데 오류가 생길수도 있음
