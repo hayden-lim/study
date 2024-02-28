d9ef1dee2b666fd131b65d2e5c96e0b9b5f61794
hayden-lim <hahyunlim.90@gmail.com>
2024-02-28 23:43:52 +0900

    20240228
    출장복귀

--- infllearn/java/java-start/src/ex/OperatorEx3.java
+++ infllearn/java/java-start/src/ex/OperatorEx3.java
@@ -6,6 +6,6 @@ public class OperatorEx3 {
         boolean result = score >= 80 && score <= 100;
         System.out.println(result);
 
-        //boolean = true,false 를 판단해주는 변수
+        //boolean = true,false 를 판단해주기 때문이 불린형을 사용
     }
 }
