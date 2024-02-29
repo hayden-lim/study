b6604e83424444d636238f89b8c89d6ea71afec7
hayden-lim <hahyunlim.90@gmail.com>
2024-02-29 23:28:13 +0800

    20240229
    상해 여행 첫째날

--- infllearn/java/java-start/src/cond/If3.java
+++ infllearn/java/java-start/src/cond/If3.java
@@ -5,22 +5,22 @@ public class If3 {
         int age = 14;
 
         if (age <= 7) {
-            System.out.println("미취학");
+            System.out.println(" 미취학 ");
         }
 
         if (age >= 8 && age <= 13) {
-            System.out.println("초등학생");
+            System.out.println(" 초등학생 ");
         }
 
         if (age >= 14 && age <= 16) {
-            System.out.println("중학생");
+            System.out.println(" 중학생 ");
         }
         if (age >= 17 && age <= 19) {
-            System.out.println("고등학생");
+            System.out.println(" 고등학생 ");
         }
 
         if (age >= 20) {
-            System.out.println("성인");
+            System.out.println(" 성인 ");
 
         }
 
