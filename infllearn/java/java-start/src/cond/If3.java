b205b0fd706da064df08db3d6dfe0328a24f2a1f
hayden-lim <hahyunlim.90@gmail.com>
2024-03-03 21:18:05 +0900

    20240303
    상해 마지막날

--- infllearn/java/java-start/src/cond/If3.java
+++ infllearn/java/java-start/src/cond/If3.java
@@ -6,22 +6,30 @@ public class If3 {
 
         if (age <= 7) {
             System.out.println(" 미취학 ");
+        
         }
+
         if (age >= 8 && age <= 13) {
             System.out.println(" 초등학생 ");
+
         }
+
         if (age >= 14 && age <= 16) {
             System.out.println(" 중학생 ");
+
         }
+
         if (age >= 17 && age <= 19) {
             System.out.println(" 고등학생 ");
+
         }
+
         if (age >= 20) {
             System.out.println(" 성인 ");
+
         }
 
-
-        // 이렇게 하면 질문이 중복되는 느낌이 있음 (실무에서는 쓸모없이 실행이 반복됨)
+        // 이런식으로 풀어서 사용하면 질문이 중복되는 느낌이 있음 (실무에서는 쓸모없이 실행이 반복됨)
         // else if문을 사용하면 조건을 만족했을 떄 그 이후는 코드 실행하지 않음
 
 
