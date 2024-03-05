a4f6a1eae5f877e1d36575d33efd1d0d2e677037
hayden-lim <hahyunlim.90@gmail.com>
2024-03-05 23:38:09 +0900

    20240305
    오늘까지만 쉬어가자,,,

--- infllearn/java/java-start/src/cond/If2.java
+++ infllearn/java/java-start/src/cond/If2.java
@@ -8,10 +8,10 @@ public class If2 {
         int age = 17;
 
         if (age >= 18) {
-            System.out.println("성인입니다");
+            System.out.println("당신은 성인입니다");
 
         } else {
-            System.out.println("미성년자입니다");
+            System.out.println("당신은     미성년자입니다");
 
         }
 
