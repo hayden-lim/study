2f91cb44586bc3225166060d485502692d06d87f
hayden-lim <hahyunlim.90@gmail.com>
2024-03-06 22:06:11 +0900

    20240306

--- infllearn/java/java-start/src/cond/If2.java
+++ infllearn/java/java-start/src/cond/If2.java
@@ -11,7 +11,7 @@ public class If2 {
             System.out.println("당신은 성인입니다");
 
         } else {
-            System.out.println("당신은     미성년자입니다");
+            System.out.println("당신은 미성년자입니다");
 
         }
 
