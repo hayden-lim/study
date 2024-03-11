6cf4ed9b2157e23dedb4f001bc4073132876ccc0
hayden-lim <hahyunlim.90@gmail.com>
2024-03-11 22:04:21 +0900

    20240311

--- infllearn/java/java-start/src/cond/IforElseif.java
+++ infllearn/java/java-start/src/cond/IforElseif.java
@@ -3,15 +3,14 @@ package cond;
 public class IforElseif {
     public static void main(String[] args) {
         /*
-        
+
         //예시 1 if-else if 사용 : 서로 연관된 조건, if를 먼저 수행하고 그 다음에 else if를 수행할 때
         if (condition1) {
             // 작업 1 수행
         } else if (condition2) {
             // 작업 2 수행
-
         }
-        //예시2 if 각각 사용 : 독립 조건일 때
+        //예시2 if 각각 사용 : 독립 조건일 때 작업1도 수행하고, 그다음 모두 작업2도 수행한다
 
         if (condition1) {
             // 작업 1 수행
