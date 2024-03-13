d1dbbfba832cdf3f60d3d016c37251ce0a120ef8
hayden-lim <hahyunlim.90@gmail.com>
2024-03-13 23:26:59 +0900

    20240313

--- infllearn/java/java-start/src/cond/IforElseif.java
+++ infllearn/java/java-start/src/cond/IforElseif.java
@@ -7,8 +7,10 @@ public class IforElseif {
         //예시 1 if-else if 사용 : 서로 연관된 조건, if를 먼저 수행하고 그 다음에 else if를 수행할 때
         if (condition1) {
             // 작업 1 수행
+
         } else if (condition2) {
             // 작업 2 수행
+
         }
         //예시2 if 각각 사용 : 독립 조건일 때
 
@@ -19,6 +21,7 @@ public class IforElseif {
             //작업 2 수행
 
         }
+
 
     }
 }
